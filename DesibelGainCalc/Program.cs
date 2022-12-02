6c8328715fecb9852e9248bf14df3b70e784a8c6
SpaceBeeGaming <space.bee.gaming@gmail.com>
2022-12-02 15:40:08 +0200

    Improvements

--- DesibelGainCalc/Program.cs
+++ DesibelGainCalc/Program.cs
@@ -2,103 +2,73 @@
 
 while (true)
 {
-    Console.Write("Gain: ");
-    string? inputstring = Console.ReadLine();
-    if (Int32.TryParse(inputstring, out int input) is false)
+    int input;
+    do
     {
-        Console.WriteLine("Not A Number '{0}'", inputstring);
-        Console.WriteLine("Press 'Enter' to Continue...");
-        Console.ReadLine();
-        continue;
-    }
+        Console.Write("Gain: ");
+        string? inputstring = Console.ReadLine();
+        if (Int32.TryParse(inputstring, out input))
+        {
+            break;
+        }
 
-    Console.Write("Gain Unit (dBW, dBm, dBf, dBV, dBµV): ");
-    string? referenceUnit = Console.ReadLine();
-    if (String.IsNullOrWhiteSpace(referenceUnit))
+        if (Console.IsInputRedirected && inputstring is null)
+        {
+            throw new InvalidOperationException("Input has no lines available.");
+        }
+
+        Console.WriteLine($"Not A Number '{inputstring}'");
+    } while (true);
+
+    string? referenceUnit;
+    do
     {
+        Console.Write("Gain Unit (dBW, dBm, dBf, dBV, dBµV): ");
+        referenceUnit = Console.ReadLine();
+
+        if (referenceUnit is "dBW" or "dBm" or "dBf" or "dBV" or "dBµV")
+        {
+            break;
+        }
+
+        if (Console.IsInputRedirected && referenceUnit is null)
+        {
+            throw new InvalidOperationException("Input has no lines available.");
+        }
+
         Console.WriteLine("Gain unit not given.");
-        Console.WriteLine("Press 'Enter' to Continue...");
-        Console.ReadLine();
-        continue;
-    }
+    } while (true);
 
-    int whole10 = input / 10;
-
-    int rem10 = input % 10;
-    int whole3 = rem10 / 3;
-
-    int rem3 = rem10 % 3;
+    (int whole10, int rem10) = Math.DivRem(input, 10);
+    (int whole3, int rem3) = Math.DivRem(rem10, 3);
 
     double multiplier = Math.Pow(10, whole10) * Math.Pow(2, whole3);
-    switch (rem3)
+
+    multiplier *= rem3 switch
     {
-        // 10 dB = 10 multiplier
-        // + 3 dB = multiplier*2
-        // - 3 dB = multiplier/2
-        case -2:
-            // Add -2 dB
+        -2 => 0.625,    // Add -2 dB = 1 dB - 3 dB => 1.25/2 = 0.625.
+        -1 => 0.78125,  // Add -1 dB = 2dB - 3 dB
+        0 => 1,         // Do Nothing.
+        1 => 1.25,      // Add 1 dB = 10 dB - 3 dB - 3 dB - 3 dB => 10/2/2/2 = 1.25
+        2 => 1.5625,    // Add 2 dB = 1 dB + 1 dB => 1.25^2 = 1.5625.
+        _ => throw new InvalidOperationException("x mod 3 returned unexpected value.")
+    };
 
-            // -2 dB = 1 dB - 3 dB => 1.25/2 = 0.675 multiplier.
-            multiplier *= 0.675;
-            break;
-        case -1:
-            // Add -1 dB
-
-            // -1 dB = 2dB - 3 dB
-            multiplier *= Math.Pow(1.25, 2) / 2;
-            break;
-        case 0:
-            // Do Nothing.
-            break;
-        case 1:
-            // Add 1 dB.
-
-            // 4 dB = 10 dB - 3 dB - 3 dB => 10/2/2 = 2,5 multiplier.
-            // 1 dB = 4 dB - 3 dB => 2,5/2 = 1.25 multiplier.
-            multiplier *= 1.25;
-            break;
-        case 2:
-            // Add 2 dB.
-            // 2 dB = 1 dB + 1 dB => 1.25^2 = 1.1625 multiplier.
-            multiplier *= Math.Pow(1.25, 2);
-            break;
-        default:
-            throw new InvalidOperationException("x mod 3 returned unexpected value.");
-    }
-    double referenceUnitValue;
-    string outUnit;
-    string referenceUnitIdentifier = referenceUnit.Length >= 3 ? referenceUnit.Substring(2) : referenceUnit;
-    switch (referenceUnitIdentifier)
+    (double referenceUnitValue, string outUnit) = referenceUnit[2..] switch
     {
-        case "W":
-            referenceUnitValue = 1;
-            outUnit = "W";
-            break;
-        case "m":
-            referenceUnitValue = Math.Pow(10, -3);
-            outUnit = "mW";
-            break;
-        case "f":
-            referenceUnitValue = Math.Pow(10, -15);
-            outUnit = "fW";
-            break;
-        case "V":
-            referenceUnitValue = 1;
-            outUnit = "V";
-            break;
-        case "µV":
-            referenceUnitValue = Math.Pow(10, -6);
-            outUnit = "µV";
-            break;
-        default:
-            Console.WriteLine("Invalid Reference value type '{0}'", referenceUnit);
-            continue;
-    }
+        "W" => (1, "W"),
+        "m" => (1.0E-3, "mW"),
+        "f" => (1.0E-15, "fW"),
+        "V" => (1, "V"),
+        "µV" => (1.0E-6, "µV"),
+        _ => throw new InvalidOperationException("Shouldn't be possible.")
+    };
+
     double outPower = referenceUnitValue * multiplier;
 
     Console.WriteLine();
-    Console.WriteLine("Multiplier: {0}", multiplier);
-    Console.WriteLine("Output Power: {0} {1}", outPower, outUnit);
+    Console.WriteLine($"Multiplier: {multiplier}");
+    Console.WriteLine($"Output Power: {outPower} {outUnit}");
     Console.WriteLine();
     Console.WriteLine("Press 'Ctrl+C' to exit.");
     Console.WriteLine();
